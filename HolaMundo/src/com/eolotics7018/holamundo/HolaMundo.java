5bed63875f26c2931e9de11dc1fa02724ba84c34
EduardoRasgado <eduardo.rasgado@hotmail.com>
2018-08-20 01:44:33 -0500

    HolaMundo project with Eclipse

---
+++ HolaMundo/src/com/eolotics7018/holamundo/HolaMundo.java
@@ -0,0 +1,10 @@
+package com.eolotics7018.holamundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Java!");
+	}
+
+}
