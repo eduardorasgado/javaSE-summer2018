581511cd706cb6309221465546b9a4597e02f286
EduardoRasgado <eduardo.rasgado@hotmail.com>
2018-08-20 04:12:23 -0500

    castings

--- Castings/src/com/eolotics7018/castings/Castings.java
+++ Castings/src/com/eolotics7018/castings/Castings.java
@@ -5,6 +5,34 @@ public class Castings {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Again a hello Java :)");
+		
+		// casteo implicito
+		//pasar de byte a short
+		byte b = 6;
+		short s = b;
+		
+		// ahora para pasar de un dato de mayor embergadura
+		// a uno de menor, debemos de usar un cast
+		
+		// Esto no es posible entonces castearse asi
+		//b = s;
+		
+		b = (byte) s;
+		System.out.println(b);
+		
+		// Otro ejemplo
+		int a = 1;
+		double d = 2.5;
+		
+		// de neuvo esto no se puede
+		//a = d;
+		
+		// casteo
+		a = (int) d;
+		
+		// ojo, puede existir perdida de datos
+		// el dato se puede truncar
+		System.out.println(a);
 	}
 
 }
