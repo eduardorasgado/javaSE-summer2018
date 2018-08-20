003d2f45538434dec693f0f0a80c4ecdada584bf
EduardoRasgado <eduardo.rasgado@hotmail.com>
2018-08-20 03:24:05 -0500

    data types

--- HolaMundo/src/com/eolotics7018/holamundo/HolaMundo.java
+++ HolaMundo/src/com/eolotics7018/holamundo/HolaMundo.java
@@ -5,6 +5,12 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Java!");
+		
+		// Enteros
+		byte edad = 127; // +- 128
+		short year = 32767; // +- 32768
+		int idUser = 1001;
+		long id_twitter = 2342343543245983L; // Al final debemos agregar una L
 	}
 
 }
