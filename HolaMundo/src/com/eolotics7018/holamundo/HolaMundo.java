44eab64fbfb0a28617b0847ebe3c1b5b4e324b4c
EduardoRasgado <eduardo.rasgado@hotmail.com>
2018-08-20 03:40:36 -0500

    boleans and text

--- HolaMundo/src/com/eolotics7018/holamundo/HolaMundo.java
+++ HolaMundo/src/com/eolotics7018/holamundo/HolaMundo.java
@@ -11,6 +11,18 @@ public class HolaMundo {
 		short year = 32767; // +- 32768
 		int idUser = 1001;
 		long id_twitter = 2342343543245983L; // Al final debemos agregar una L
+		
+		// Punto flotante
+		float diametro = 34.25F; // la F indica que lo tome com Float y no double
+		double precio = 1254.222356432452;
+		
+		// Texto
+		char generoFemenino = 'F';
+		char generoMasculino = 'M';
+		
+		// booleanos o logicos
+		boolean isVisible = true;
+		boolean funciona = false;
 	}
 
 }
