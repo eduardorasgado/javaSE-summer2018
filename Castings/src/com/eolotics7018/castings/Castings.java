487a2c526c753db97dedde99a933d4cfc8a3540e
EduardoRasgado <eduardo.rasgado@hotmail.com>
2018-08-20 04:05:28 -0500

    new casting lessons

---
+++ Castings/src/com/eolotics7018/castings/Castings.java
@@ -0,0 +1,10 @@
+package com.eolotics7018.castings;
+
+public class Castings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Again a hello Java :)");
+	}
+
+}
