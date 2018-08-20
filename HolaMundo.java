141f1980e7f402532e3feb81c64c97df1f991ea0
EduardoRasgado <eduardo.rasgado@hotmail.com>
2018-08-19 21:45:15 -0500

    Hello world java

---
+++ HolaMundo.java
@@ -0,0 +1,8 @@
+public class HolaMundo {
+	public static void main(String[] args) {
+		// Comentario:
+		// compilar con javac HolaMundo.java
+		// ejecutar con java HolaMundo
+		System.out.println("Hola Java");
+	}
+}
