d9cd854ac10509d264a67c9f717ccde6fa85a75e
EduardoRasgado <eduardo.rasgado@hotmail.com>
2018-08-21 23:24:46 -0500

    accesing to arrays

--- Arrays/src/com/Eolotics7018/arrays/Arrays.java
+++ Arrays/src/com/Eolotics7018/arrays/Arrays.java
@@ -14,8 +14,36 @@ public class Arrays {
 		
 		int arrayTwoDim[][] = new int[2][3]; // arreglo de 2 x 3
 		
-		int array3D[][][] = new int[3][3][2];		
+		int array3D[][][] = new int[3][3][2]; // en este arreglo caben 18 elementos
 		
+		int array4D[][][][] = new int[3][3][3][3]; // 4 dimensiones
+		
+		// en vez de new va '{}'
+		// Este arreglo puede ir creciendo
+		char[][] daysArray = {{'M','T', 'W'},{'M','T','W'}};
+		
+		// ARREGLO DE TRES DIMENSIONES
+		char channelArray[][][] = {
+									{{'A','B','C'},{'D','E'},{'F'}},
+									{{'G','H','I'},{'J','K'},{'L'}},
+									{{'N','O','P'},{'Q','R'},{'S'}}
+									};
+		
+		// acceder a C
+		System.out.println(channelArray[0][0][2]);
+		// cambiando el valor de ese elemento
+		channelArray[0][0][2] = 'X';
+		System.out.println(channelArray[0][0][2]);
+		
+		char names[] = new char[3];
+		
+		names[0] = 'h';
+		names[1] = 'e';
+		names[2] = 'y';
+		
+		for (int i = 0; i < names.length; i++) {
+			System.out.println(names[i]);
+		}
 	}
 
 }
