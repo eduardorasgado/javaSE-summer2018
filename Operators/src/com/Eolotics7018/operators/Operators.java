56334b4b8239c35bdd908490de35778ea8a7324d
EduardoRasgado <eduardo.rasgado@hotmail.com>
2018-08-21 23:58:50 -0500

    operators lesson

---
+++ Operators/src/com/Eolotics7018/operators/Operators.java
@@ -0,0 +1,29 @@
+package com.Eolotics7018.operators;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		// Operadores aritmeticos
+		System.out.println("Operadores Aritméticos");
+		int a = 1;
+		int aa = a+a;
+		// concatenacion
+		System.out.println("El valor de sumar "+a+" mas "+a+" es "+aa);
+		
+		double x = 2.56;
+		int y = 9;
+		
+		// suma
+		float w = (float) x + y;
+		System.out.println(w);
+		System.out.println(w*2);
+		
+		// division
+		double k = 4 / 0.00002;
+		System.out.println(k);
+		
+		//modulo
+		System.out.println(7%2);
+	}
+
+}
