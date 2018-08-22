e60cbadd67086e4c47a2bd41fb520491808825a8
EduardoRasgado <eduardo.rasgado@hotmail.com>
2018-08-22 02:40:28 -0500

    do while to main program showing

--- AmazonViewer/src/com/Eolotics7018/amazonviewer/Main.java
+++ AmazonViewer/src/com/Eolotics7018/amazonviewer/Main.java
@@ -4,15 +4,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		// program main function
-		System.out.println("");
-		System.out.println("****BIENVENIDO A AMAZON VIEWER****");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("Selecciona el número de la opción deseada");
-		System.out.println("1. Movies");
-		System.out.println("2. Series");
-		System.out.println("3. Books");
-		System.out.println("4. Magazines");
+		
+		// variable para salir
+		int exit = 0;
+		
+		// bucle de ejecucion inicial
+		do {
+			System.out.println("");
+			System.out.println("****BIENVENIDO A AMAZON VIEWER****");
+			System.out.println("");
+			System.out.println("");
+			System.out.println("Selecciona el número de la opción deseada");
+			System.out.println("1. Movies");
+			System.out.println("2. Series");
+			System.out.println("3. Books");
+			System.out.println("4. Magazines");
+			System.out.println("0. Exit");
+		} while (exit != 0);
 	}
 
 }
