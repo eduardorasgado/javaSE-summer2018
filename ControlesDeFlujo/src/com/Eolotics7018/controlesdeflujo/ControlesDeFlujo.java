2dc5337927fb35687a6276ed34a18c03fc3ff416
EduardoRasgado <eduardo.rasgado@hotmail.com>
2018-08-22 03:55:59 -0500

    for loop

--- ControlesDeFlujo/src/com/Eolotics7018/controlesdeflujo/ControlesDeFlujo.java
+++ ControlesDeFlujo/src/com/Eolotics7018/controlesdeflujo/ControlesDeFlujo.java
@@ -69,6 +69,8 @@ public class ControlesDeFlujo {
 		// While loop ********************************************************************
 		
 		// While
+		System.out.println("CICLO WHILE");
+		
 		int e = 1;
 		while (e <= 5) {
 			System.out.println("e es igual o menor a 5, e es: " + e);
@@ -78,10 +80,32 @@ public class ControlesDeFlujo {
 		
 		// Do while
 		// Este flujo se ejecuta al menos una vez
+		System.out.println("CICLO DO WHILE");
+		
 		int k = 0;
 		do {
 			System.out.println("k es igual o menor a 5, k es: " + k);
 			k++;
 		} while (k<=5);
+		System.out.println("Este flujo continua...2");
+		
+		// for loop
+		System.out.println("CICLO FOR");
+		
+		 for (int c = 0; c <= 5; c++) {
+			 System.out.println("c es igual o menor a 5, k es: " + c);
+		 }
+		 
+		 System.out.println("");
+		 int numeros[] = new int[5];
+		 
+		 // agregando elementos al arreglo
+		 for(int i = 0; i < 5; i++) {
+			 numeros[i] = 1;
+		 }
+		 // imprimiendo los datos del arreglo
+		 for(int i = 0; i < 5; i++) {
+			 System.out.println(numeros[i]);
+		 }
 	}
 }
