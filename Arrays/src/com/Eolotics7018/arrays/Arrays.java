f820186a9fddca13882406b779bceb6408a98c04
EduardoRasgado <eduardo.rasgado@hotmail.com>
2018-08-21 22:28:26 -0500

    multidimension arrays

--- Arrays/src/com/Eolotics7018/arrays/Arrays.java
+++ Arrays/src/com/Eolotics7018/arrays/Arrays.java
@@ -5,7 +5,17 @@ public class Arrays {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Let's talk about arrays");
-
+		
+		// Arrays
+		// Declaracion de arreglos
+		int[] arregloInts = new int[3]; //arreglos de tres elementos
+				
+		double arregloDouble[] = new double[3];
+		
+		int arrayTwoDim[][] = new int[2][3]; // arreglo de 2 x 3
+		
+		int array3D[][][] = new int[3][3][2];		
+		
 	}
 
 }
