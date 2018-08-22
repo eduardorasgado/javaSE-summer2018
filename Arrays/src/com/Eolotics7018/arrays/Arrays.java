73dcbe748fee93fc6e9b462d1a0d02812844daa9
EduardoRasgado <eduardo.rasgado@hotmail.com>
2018-08-21 20:43:21 -0500

    arrays class created

---
+++ Arrays/src/com/Eolotics7018/arrays/Arrays.java
@@ -0,0 +1,11 @@
+package com.Eolotics7018.arrays;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Let's talk about arrays");
+
+	}
+
+}
