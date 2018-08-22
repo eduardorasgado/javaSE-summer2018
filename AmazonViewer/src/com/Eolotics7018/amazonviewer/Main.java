71efa63ef1f30b7a57633282d5d43bbebc2e0553
EduardoRasgado <eduardo.rasgado@hotmail.com>
2018-08-22 02:37:21 -0500

    main menu wrote

--- AmazonViewer/src/com/Eolotics7018/amazonviewer/Main.java
+++ AmazonViewer/src/com/Eolotics7018/amazonviewer/Main.java
@@ -3,8 +3,16 @@ package com.Eolotics7018.amazonviewer;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// program main function
+		System.out.println("");
+		System.out.println("****BIENVENIDO A AMAZON VIEWER****");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("Selecciona el número de la opción deseada");
+		System.out.println("1. Movies");
+		System.out.println("2. Series");
+		System.out.println("3. Books");
+		System.out.println("4. Magazines");
 	}
 
 }
